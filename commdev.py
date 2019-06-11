8287cab20010e807d7b5e14f104f5da79603dee6
Brendon Barnes <brenbrenofspace@gmail.com>
2019-06-11 08:37:34 -0400

    i guess i never miss, huh?

--- commdev.py
+++ commdev.py
@@ -9,6 +9,11 @@ data=json.load(r)
 ConfigJSON=list(data.values())
 TOKEN=ConfigJSON[0]
 bot = commands.Bot(command_prefix='>') #allows you to change the bot's prefix
+client = discord.Client()
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
 @bot.command()
 async def repeat(ctx, arg):
