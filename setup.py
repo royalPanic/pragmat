fffd1c6f4387a5238b5652cd77894f149ab6a75e
Brendon Barnes <brenbrenofspace@gmail.com>
2019-06-12 11:38:30 -0400

    fixing build

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-# I have absolutely no idea what to put in this.
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command','discord'],
+     )
