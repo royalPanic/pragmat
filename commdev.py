974d94631092c08603b16a976eed0955d393cd23
Brendon Barnes <brenbrenofspace@gmail.com>
2019-06-13 08:59:28 -0400

    QoL for DM logging

--- commdev.py
+++ commdev.py
@@ -59,7 +59,7 @@ async def setprefix(ctx, prefix: str=None):
 @bot.event # Hopefully handles DMs while bot is online
 async def on_message(message):
     if isinstance(message.channel, discord.DMChannel):
-        print(f'{message.content} prviate message from {message.author}')
+        print(f'Prviate message from {message.author}: "{message.content}"')
     await bot.process_commands(message)
 
 
